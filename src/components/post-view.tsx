ab9ab943cd9cde9c0762796fc5cf017bf532eac6
Ortem Titov <titovortem@gmail.com>
2026-02-12 22:09:42 +0000

    он все еще дублируется

--- src/components/post-view.tsx
+++ src/components/post-view.tsx
@@ -38,7 +38,7 @@ export function PostView({ post, author }: { post: Post, author: UserProfile | n
                                  <Avatar className="h-10 w-10">
                                     <AvatarImage src={author.profilePictureUrl ?? undefined} alt={author.nickname} />
                                     <AvatarFallback>{author.nickname[0].toUpperCase()}</AvatarFallback>
-                                </Avatar>
+                                 </Avatar>
                             </Link>
                             <div className="flex-1 min-w-0">
                                 <p className="text-sm font-semibold text-foreground truncate">
@@ -53,9 +53,11 @@ export function PostView({ post, author }: { post: Post, author: UserProfile | n
                 </div>
 
                 <div className="p-4 flex-1 overflow-y-auto">
-                     <p className="text-sm text-foreground break-words">
-                        {post.caption}
-                    </p>
+                    {mediaUrl && post.caption && (
+                        <p className="text-sm text-foreground break-words">
+                            {post.caption}
+                        </p>
+                    )}
                 </div>
 
                 <div className="p-4 border-t text-center text-muted-foreground text-sm">
